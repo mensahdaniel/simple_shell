4f1258f51eb22aa6d32dd430837a0a05968e795c
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-15 09:42:09 +0000

    Nov 15, 2023, 9:42 AM

--- cmd_tokenizer.c
+++ cmd_tokenizer.c
@@ -3,7 +3,7 @@
 char **tokenizer(char *lineptr)
 {
 	int i;
-	char *command, **cmds = args;
+	char *command, **cmds = args{NULL, NULL};
 	for (i = 0; lineptr[i] != '\n'; i++)
 		;
 	lineptr[i] = '\0';
