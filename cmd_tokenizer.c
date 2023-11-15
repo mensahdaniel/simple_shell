e597ea33a3cf57a4eeacc5873ad8c809c70e5338
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-15 12:12:30 +0000

    Nov 15, 2023, 12:12 PM

--- cmd_tokenizer.c
+++ cmd_tokenizer.c
@@ -10,18 +10,18 @@
 char **tokenizer(char *buffer, char *delim)
 {
 	int i;
-	char *command, /**tokens,*/ *args[SIZE] = {NULL, NULL};
+	char *command, **tokens;
 
 	command = _strtok(buffer, delim);
 	i = 0;
-	/*tokens = args;*/
+	tokens = args;
 	while (command != NULL)
 	{
-		args[i] = command;
+		tokens[i] = command;
 		command = _strtok(NULL, delim);
 		i++;
 	}
-	args[i] = NULL;
+	tokens[i] = NULL;
 
-	return (args);
+	return (tokens);
 }
