040347c85e7b886a669cb62cca22d18da39b27a1
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-16 09:14:28 +0000

    Nov 16, 2023, 9:14 AM

--- more_funcs.c
+++ more_funcs.c
@@ -4,35 +4,24 @@ char *_itoa(int num)
 {
 	int temp = num, n_digits = 0;
 	int isNegative = 0, dex;
+	char *str = (char *)malloc(12 * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
 
 	if (num < 0)
 	{
-		isNegative = 1;
 		temp = -temp;
+		str[0] = '-';
+	}
+	else
+	{
 	}
 
 	while (temp != 0)
 	{
 		n_digits++;
 		temp /= 10;
-	}
-
-	char *str = (char *)malloc(12 * sizeof(char));
-
-	if (str == NULL)
-		return (NULL);
-
-	if (isNegative)
-		str[0] = '-';
-
-	dex = n_digits + isNegative;
-	str[dex] = '\0';
-
-	while (n_digits != 0)
-	{
-		str[--dex] = (num % 10) + '0';
-		num /= 10;
-		n_digits--;
 	}
 
 	return (str);
