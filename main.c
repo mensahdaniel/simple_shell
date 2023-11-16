9b2914b90f071b844611b513763fcada865453f9
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-16 21:21:51 +0000

    Nov 16, 2023, 9:21 PM

--- main.c
+++ main.c
@@ -41,8 +41,8 @@ int main(int ac, char **argv)
 
 		cmds = tokenizer(lineptr, " ");
 		execute(cmds, argv, count);
+		free(cmds);
 	}
-
 	free(lineptr); // Free memory allocated by getline after processing
 	return 0;
 }
