2a80ab110cef420473c095d01e5ff57d5d650d7a
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 08:04:05 +0000

    Nov 17, 2023, 8:04 AM

--- print_func.c
+++ print_func.c
@@ -1,4 +1,27 @@
 #include "main.h"
+
+/**
+ *  _prerror - Print Custome Error
+ * @argv:Program Name
+ * @c:Error Count
+ * @cmd:Command
+ * Return: Void
+ */
+void _prerror(char **argv, int c, char **cmd)
+{
+	char *er = _itoa(c);
+
+	PRINT(argv[0]);
+	PRINT(": ");
+	PRINT(er);
+	PRINT(": ");
+	PRINT(cmd[0]);
+	PRINT(": Illegal number: ");
+	PRINT(cmd[1]);
+	PRINT("\n");
+	free(er);
+}
+
 /**
  * print_number -Print Unsigned Int Putchar
  * @n: Unisigned Integer
