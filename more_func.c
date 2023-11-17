0fc0d0e36d43c9603e76632807f6d3120de1627e
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 09:43:32 +0000

    Nov 17, 2023, 9:43 AM

--- more_func.c
+++ more_func.c
@@ -64,38 +64,34 @@ char *_itoa(size_t num)
 
 	if (str == NULL)
 		exit(1);
-	/* Handle negative numbers */
-	if (num < 0)
+
+	/* Process 0 as a special case */
+	if (num == 0)
 	{
-		isNegative = 1;
-		num = -num;
+		str[i++] = '0';
+		str[i] = '\0';
+		return str;
 	}
+
 	/* Process individual digits */
 	while (num != 0)
 	{
 		str[i++] = num % 10 + '0'; /* Convert digit to character */
 		num = num / 10;
 	}
-	/*Add '-' for negative numbers */
-	if (isNegative)
-		str[i++] = '-';
-	/*If the number is 0 */
-	if (i == 0)
-		str[i++] = '0';
-	str[i] = '\0'; /* Adding a null terminator*/
 
 	/* Reverse the string */
 	int start = 0, end = i - 1;
-
 	while (start < end)
 	{
 		char temp = str[start];
-
 		str[start] = str[end];
 		str[end] = temp;
 		start++;
 		end--;
 	}
 
-	return (str);
+	str[i] = '\0'; /* Adding a null terminator */
+
+	return str;
 }
