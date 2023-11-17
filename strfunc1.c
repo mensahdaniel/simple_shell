8f6f03ef8fa1cc5d60f6d2a70364c50426b43ccf
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 08:44:30 +0000

    Nov 17, 2023, 8:44 AM

--- strfunc1.c
+++ strfunc1.c
@@ -37,3 +37,35 @@ char *_strtok(char *str, const char *delim)
 
 	return (tokenStart);
 }
+
+/**
+ * _strcmp - Compare Two String
+ * @s1:String 1
+ * @s2:String 2
+ * Return: 0 If Identical Otherwise How Much Diffrent
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int cmp = 0, i, len1, len2;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (s1 == NULL || s2 == NULL)
+		return (1);
+
+	if (len1 != len2)
+		return (1);
+
+	for (i = 0; s1[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			cmp = s1[i] - s2[i];
+			break;
+		}
+		else
+			continue;
+	}
+	return (cmp);
+}
--- strfunc2.c
+++ strfunc2.c
@@ -98,34 +98,3 @@ char *_strdup(char *str)
 
 	return (str2);
 }
-
-/**
- * _strcmp - Compare Two String
- * @s1:String 1
- * @s2:String 2
- * Return: 0 If Identical Otherwise How Much Diffrent
- */
-int _strcmp(char *s1, char *s2)
-{
-	int cmp = 0, i, len1, len2;
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-
-	if (s1 == NULL || s2 == NULL)
-		return (1);
-
-	if (len1 != len2)
-		return (1);
-
-	for (i = 0; s1[i]; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			cmp = s1[i] - s2[i];
-			break;
-		}
-		else
-			continue;
-	}
-	return (cmp);
-}
