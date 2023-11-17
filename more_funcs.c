b4300dea35b0b06caba86edb589c3ec3267e8ba6
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 05:51:22 +0000

    Nov 17, 2023, 5:51 AM

--- more_funcs.c
+++ more_funcs.c
@@ -8,36 +8,58 @@
  */
 char *_itoa(int num)
 {
-	int temp, dex = 10;
-	char *str = malloc(12 * sizeof(char *));
+	/* The Maximum length of an integer in string form is 11 characters including sign */
+	/* 10 digits + 1 for sign */
+	char *str = (char *)malloc(12 * sizeof(char)); // Allocating memory for the string
 
 	if (str == NULL)
-		return (NULL);
+	{
+		printf("Memory allocation failed.\n");
+		exit(1);
+	}
 
+	int i = 0;
+	int isNegative = 0;
+
+	/* Handle negative numbers */
 	if (num < 0)
 	{
-		temp = -num;
-		str[0] = '-';
-	}
-	else
-	{
-		temp = num;
-		str[0] = '\0';
+		isNegative = 1;
+		num = -num;
 	}
 
-	if (temp == 0)
+	/* Process individual digits */
+	while (num != 0)
 	{
-		str[dex--] = '0';
+		str[i++] = num % 10 + '0'; /* Convert digit to character */
+		num = num / 10;
 	}
-	else
-	{
-		while (temp != 0)
-		{
-			char digit = (char)(temp % 10) + '0';
 
-			str[dex--] = digit;
-			temp /= 10;
-		}
+	/*Add '-' for negative numbers */
+	if (isNegative)
+	{
+		str[i++] = '-';
 	}
-	return (&str[dex + 1]);
+
+	/*If the number is 0 */
+	if (i == 0)
+	{
+		str[i++] = '0';
+	}
+
+	str[i] = '\0'; /* Adding a null terminator*/
+
+	/* Reverse the string */
+	int start = 0;
+	int end = i - 1;
+	while (start < end)
+	{
+		char temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
+
+	return str;
 }
