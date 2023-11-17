a7f64cee85fabf1cbd758e23b9dedbbcd35a733e
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 08:06:23 +0000

    Nov 17, 2023, 8:06 AM

--- strfunc1.c
+++ strfunc1.c
@@ -1,64 +1,39 @@
 #include "main.h"
 /**
- * check_delim - Checks If A Character Match Any Char *
- * @c: Character To Check
- * @str: String To Check
- * Return: 1 Succes, 0 Failed
- */
-unsigned int check_delim(char c, const char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (c == str[i])
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * _strtok - Token A String Into Token (strtrok)
- * @str: String
- * @delim: Delimiter
- * Return: Pointer To The Next Token Or NULL
+ * _strtok - Tokenize a string based on a specific delimiter
+ *
+ * @str: The string to tokenize
+ * @delim: The delimiter to be used to tokenize the string
+ * Return: char* The tokenized string
  */
 char *_strtok(char *str, const char *delim)
 {
-	static char *ts;
-	static char *nt;
-	unsigned int i;
+	static char *nextToken = NULL, *tokenStart;
+
+	if (str == NULL && nextToken == NULL)
+		return (NULL);
 
 	if (str != NULL)
-		nt = str;
-	ts = nt;
-	if (ts == NULL)
-		return (NULL);
-	for (i = 0; ts[i] != '\0'; i++)
+		nextToken = str;
+
+	while (_strchr(delim, *nextToken) != NULL)
+		nextToken++;
+
+	if (*nextToken == '\0')
 	{
-		if (check_delim(ts[i], delim) == 0)
-			break;
-	}
-	if (nt[i] == '\0' || nt[i] == '#')
-	{
-		nt = NULL;
+		nextToken = NULL;
 		return (NULL);
 	}
-	ts = nt + i;
-	nt = ts;
-	for (i = 0; nt[i] != '\0'; i++)
+	tokenStart = nextToken;
+
+	while (*nextToken != '\0' && _strchr(delim, *nextToken) == NULL)
+		nextToken++;
+
+	if (*nextToken != '\0')
 	{
-		if (check_delim(nt[i], delim) == 1)
-			break;
+		*nextToken = '\0';
+		nextToken++;
 	}
-	if (nt[i] == '\0')
-		nt = NULL;
-	else
-	{
-		nt[i] = '\0';
-		nt = nt + i + 1;
-		if (*nt == '\0')
-			nt = NULL;
-	}
-	return (ts);
+
+	return (tokenStart);
 }
