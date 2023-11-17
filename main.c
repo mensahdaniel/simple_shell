ca671b75a50b173da139f5246c66c1ac059886e4
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 10:42:41 +0000

    Nov 17, 2023, 10:42 AM

--- main.c
+++ main.c
@@ -17,24 +17,6 @@ int main(int ac, char **argv)
 	while (1)
 	{
 		isatty_handler();
-		n_chars = getline(&lineptr, &n_size, stdin);
-
-		if (n_chars == -1)
-		{
-			free(lineptr); /* Free memory allocated by getline*/
-			exit(0);
-		}
-
-		for (i = 0; lineptr[i] != '\n'; i++)
-			;
-
-		lineptr[i] = '\0';
-
-		while (*lineptr == ' ' || *lineptr == '\t')
-			lineptr++;
-
-		if (lineptr[0] == ' ' || lineptr[0] == '\t' || lineptr[0] == '\0')
-			continue;
 
 		char **cmds;
 
