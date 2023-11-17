cd6b5887fb7ce9acbc1c41d88156bd27c31cb359
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 05:10:44 +0000

    Nov 17, 2023, 5:10 AM

--- strfunc.c
+++ strfunc.c
@@ -77,19 +77,44 @@ int _atoi(char *str)
 	}
 	return (num);
 }
-/**
- * _puts - print a string
- * @str:pointer char
- * return:void
- */
-void _puts(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+#include "main.h"
+
+/**
+ * _strtok - Tokenize a string based on a specific delimiter
+ *
+ * @str: The string to tokenize
+ * @delim: The delimiter to be used to tokenize the string
+ * Return: char* The tokenized string
+ */
+char *_strtok(char *str, const char *delim)
+{
+	static char *nextToken = NULL, *tokenStart;
+
+	if (str == NULL && nextToken == NULL)
+		return (NULL);
+
+	if (str != NULL)
+		nextToken = str;
+
+	while (_strchr(delim, *nextToken) != NULL)
+		nextToken++;
+
+	if (*nextToken == '\0')
 	{
-		_putchar(str[i]);
+		nextToken = NULL;
+		return (NULL);
 	}
-	_putchar('\n');
-	return;
+	tokenStart = nextToken;
+
+	while (*nextToken != '\0' && _strchr(delim, *nextToken) == NULL)
+		nextToken++;
+
+	if (*nextToken != '\0')
+	{
+		*nextToken = '\0';
+		nextToken++;
+	}
+
+	return (tokenStart);
 }
