707a49dd6131675513449f593dc6c59df8d40b2f
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-17 08:09:40 +0000

    Nov 17, 2023, 8:09 AM

--- strfunc3.c
+++ strfunc3.c
@@ -8,9 +8,9 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int cmp = 0, i, len1, len2;
-len1 = _strlen(s1);
-len2 = _strlen(s2);
+	int cmp = 0, i, len1, len2;
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
 	if (s1 == NULL || s2 == NULL)
 		return (1);
@@ -35,14 +35,14 @@ len2 = _strlen(s2);
  */
 int _isalpha(int c)
 {
-if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 /**
  * _itoa - Convert Integer To Char
@@ -69,38 +69,4 @@ char *_itoa(unsigned int n)
 	array_rev(s, len);
 	s[i + 1] = '\0';
 	return (s);
-}
-/**
- *  array_rev - Reverse Array
- * @arr:Array To Reverse
- * @len:Length Of Array
- * Return: Void(Reverse Array)
- */
-void array_rev(char *arr, int len)
-{
-	int i;
-	char tmp;
-
-	for (i = 0; i < (len / 2); i++)
-	{
-		tmp = arr[i];
-		arr[i] = arr[(len - 1) - i];
-		arr[(len - 1) - i] = tmp;
-	}
-}
-/**
- * intlen - Determine Length Of Int
- * @num: Given Int
- * Return: Length Of Int
- */
-int intlen(int num)
-{
-	int len = 0;
-
-	while (num != 0)
-	{
-		len++;
-		num /= 10;
-	}
-	return (len);
 }
