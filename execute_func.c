00249dc342387a47d0c7e4565e031a502d8611eb
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-19 17:50:52 +0000

    Nov 19, 2023, 5:50 PM

--- execute_func.c
+++ execute_func.c
@@ -59,7 +59,6 @@ int execute(char **cmd, char *input, int c, char **argv)
 		if (execve(*cmd, cmd, environ) == -1)
 		{
 			print_error(cmd[0], c, argv);
-			free(input);
 			free(cmd);
 			exit(EXIT_FAILURE);
 		}
