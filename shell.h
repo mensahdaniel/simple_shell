c56720b8c08e2de27e50245fed18a41930610b57
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-19 14:47:20 +0000

    Nov 19, 2023, 2:47 PM

--- shell.h
+++ shell.h
@@ -1,16 +1,6 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-/**###### environ var ######*/
-
-extern char **environ;
-
-/**##### MACROS ######*/
-
-#define BUFSIZE 1024
-#define DELIM " \t\r\n\a"
-#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
-
 /**###### LIBS USED ######*/
 
 #include <stdio.h>
@@ -24,6 +14,17 @@ extern char **environ;
 #include <fcntl.h>
 #include <errno.h>
 #include <linux/limits.h>
+
+/**###### environ var ######*/
+
+extern char **environ;
+
+/**##### MACROS ######*/
+
+#define BUFSIZE 1024
+#define DELIM " \t\r\n\a"
+#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
+
 
 
 
