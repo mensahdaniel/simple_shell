b79790cb10809cf9a5e6a53b958c6e057d0ef56c
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-20 22:14:22 +0000

    Nov 20, 2023, 10:14 PM

--- aliases_func.c
+++ aliases_func.c
@@ -4,7 +4,7 @@
 /* Helper function to handle alias commands */
 int handle_alias_command(char *buffer, alias_t **aliases)
 {
-	char *alias_prefix = "alias";
+	char *alias_prefix = "alias ";
 	size_t alias_prefix_len = _strlen(alias_prefix);
 
 	if (strncmp(buffer, alias_prefix, alias_prefix_len) == 0)
@@ -31,7 +31,7 @@ int handle_alias_command(char *buffer, alias_t **aliases)
 			return (1); /* Return 1 to indicate alias command handled */
 		}
 	}
-	else if (strcmp(buffer, "alias\n") == 0)
+	else if (strcmp(buffer, "alias") == 0)
 	{
 		/* Print aliases command */
 		print_aliases(*aliases);
