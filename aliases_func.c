4ff1b0150cae4caa22ddd87a3f739cbf1afe93ad
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-20 20:53:17 +0000

    Nov 20, 2023, 8:53 PM

---
+++ aliases_func.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * add_alias - Adds an alias to the alias list
+ *
+ * @head: Pointer to the start of the alias list
+ * @name: Name of the alias
+ * @value: Value of the alias
+ */
+void add_alias(alias_t **head, char *name, char *value)
+{
+	alias_t *new_alias = malloc(sizeof(alias_t));
+	if (!new_alias)
+	{
+		// Handle allocation failure
+		return;
+	}
+
+	new_alias->name = _strdup(name);
+	new_alias->value = _strdup(value);
+	new_alias->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_alias;
+		return;
+	}
+
+	alias_t *current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_alias;
+}
+
+/**
+ * get_alias - Gets the value of an alias
+ *
+ * @head: Pointer to the start of the alias list
+ * @name: Name of the alias
+ */
+char *get_alias(alias_t *head, char *name)
+{
+	alias_t *current = head;
+	while (current != NULL)
+	{
+		if (_strcmp(current->name, name) == 0)
+		{
+			return current->value;
+		}
+		current = current->next;
+	}
+	return NULL;
+}
+
+/**
+ * free_aliases - Frees the alias list
+ *
+ * @head: Pointer to the start of the alias list
+ */
+void free_aliases(alias_t *head)
+{
+	while (head != NULL)
+	{
+		alias_t *temp = head;
+		head = head->next;
+		free(temp->name);
+		free(temp->value);
+		free(temp);
+	}
+}
