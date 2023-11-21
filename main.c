b68ff68ef92e8dba107d1d391218251c14ea7abc
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-21 04:16:53 +0000

    Nov 21, 2023, 4:16 AM

--- main.c
+++ main.c
@@ -33,19 +33,45 @@ int main(void)
 				break;
 			continue;
 		}
-		dup = _strdup(buffer);
-		argv = tokenizer(dup, builtIn);
-		if ((builtIn == 0 && itsExecutable(argv[0]) == 0))
-			child_pid = child_fork(child_pid, argv[0]);
 		else
-			child_pid = 1;
-		if (child_pid == 0 && execve(argv[0], argv, environ) == -1)
 		{
-			perror(argv[0]);
-			break;
+			dup = _strdup(buffer);
+			argv = tokenizer(dup, builtIn);
+			char *alias_value = get_alias(aliases, argv[0]);
+			if (alias_value != NULL)
+			{
+				// If the command matches an alias, execute the alias value as a command
+				free_array_dup(argv, dup);
+				argv = tokenizer(alias_value, 0);
+				builtIn = _checkBuiltIn(alias_value); // Check if it's a built-in command
+
+				// Execute the alias value
+				child_pid = child_fork(child_pid, argv[0]);
+				if (child_pid == 0 && execve(argv[0], argv, environ) == -1)
+				{
+					perror(argv[0]);
+					break;
+				}
+				if (child_pid != 0)
+				{
+					waitAndFree(status, argv, dup);
+				}
+			}
+			else
+			{
+				// If not an alias, execute the command normally
+				child_pid = child_fork(child_pid, argv[0]);
+				if (child_pid == 0 && execve(argv[0], argv, environ) == -1)
+				{
+					perror(argv[0]);
+					break;
+				}
+				if (child_pid != 0)
+				{
+					waitAndFree(status, argv, dup);
+				}
+			}
 		}
-		if (child_pid != 0)
-			waitAndFree(status, argv, dup);
 	}
 	if (builtIn != 1)
 		free_array_dup(argv, dup);
