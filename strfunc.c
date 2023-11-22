af5f0affc7505847f029e525a55780326aa41ad7
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-22 05:09:24 +0000

    Nov 22, 2023, 5:09 AM

--- strfunc.c
+++ strfunc.c
@@ -129,3 +129,24 @@ char *_strcat(char *dest, const char *src)
 	*ptr = '\0';
 	return dest;
 }
+
+/**
+ * _strchr - Locate Charactere In String
+ * @s:String Search In
+ * @c:Char To Search For
+ * Return: Pointer To Char*
+ */
+char *_strchr(char *s, char c)
+{
+
+	do
+	{
+
+		if (*s == c)
+		{
+			break;
+		}
+	} while (*s++);
+
+	return (s);
+}
