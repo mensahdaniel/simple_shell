9b9e24680a8bd439f3ea962d0d167154a6c48398
mensahdaniel <mensahdaniel5858910@gmai.com>
2023-11-23 05:13:05 +0000

    Nov 23, 2023, 5:13 AM

--- main.c
+++ main.c
@@ -9,13 +9,13 @@
  */
 int main(int ac, char **av)
 {
-	info_t info[] = { INFO_INIT };
+	info_t info[] = {INFO_INIT};
 	int fd = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+	asm("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r"(fd)
+			: "r"(fd));
 
 	if (ac == 2)
 	{
@@ -39,6 +39,6 @@ int main(int ac, char **av)
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, av);
+	execute(info, av);
 	return (EXIT_SUCCESS);
 }
